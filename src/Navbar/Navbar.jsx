36ba2a44774074921b66978aacbeecd0caafe376
AdelAlAtram <adelaaymann@gmail.com>
2024-02-25 21:30:22 +0300

    logo

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { CiCircleList } from "react-icons/ci";
 import "./navbar.css";
+// import Logo from "../imges/Logo";
+import Logo from "../../../front/imges/Logo.png"
 const NavBar = () => {
-  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  <script
+    src="https://kit.fontawesome.com/a076d05399.js"
+    crossorigin="anonymous"
+  ></script>;
   return (
     <nav>
-    <input type="checkbox" id="check" />
-    <label for="check"  className="checkbtn">
-      <CiCircleList/>
-    </label>
-      <label  className="navbar-container"> R&D</label>
+      <input type="checkbox" id="check" />
+      <label for="check" className="checkbtn">
+        <CiCircleList />
+      </label>
+      <label className="navbar-container">
+        {" "}
+        <img src={Logo} alt="Logo" className="logo" />{" "}
+      </label>
       <ul>
         <li>
           {" "}
-          <a href="#" className="active">Home</a>{" "}
+          <a href="#" className="active">
+            Home
+          </a>{" "}
         </li>
         <li>
           {" "}
